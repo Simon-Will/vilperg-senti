8740a466d531e13da4c88c25077b55050c2c799a
Simon-Will <will@cl.uni-heidelberg.de>
2015-12-11 22:21:37 +0100

    Make write_amazon_reviews.py create a directory for each review.

--- get_amazon_reviews/write_amazon_reviews.py
+++ get_amazon_reviews/write_amazon_reviews.py
@@ -23,9 +23,9 @@ def write_products(products, out_dir):
     """Write products and their reviews to a directory structure under out_dir.
 
     For each product in the list, a directory is created under out_dir. In this
-    directory, an info file and a reviews directory are created. The info file
-    contains information about the product. The reviews directory contains
-    files describing one review each.
+    directory, product_info file and a reviews directory are created. The
+    product_info file contains information about the product. The reviews
+    directory contains files describing one review each.
 
     Args:
         products (list): A list of Amazon_product objects.
@@ -36,25 +36,32 @@ def write_products(products, out_dir):
     for p in products:
         p_dir = '{0}/{1}'.format(out_dir, p.id)
         ensure_dir(p_dir)
-        p_info = '{0}/info'.format(p_dir)
+        p_info = '{0}/product_info'.format(p_dir)
         with open(p_info, 'w') as p_f:
             p_f.write('{0}\n'.format(p))
         r_dir = '{0}/reviews'.format(p_dir)
         write_reviews(p, r_dir)
 
-def write_reviews(product, directory):
+def write_reviews(product, top_dir):
     """Write the reviews of a product in a directory.
 
     Args:
         product (Amazon_product): An Amazon_product object.
-        directory (str): The directory the reviews should be written in.
+        top_dir (str): The directory the reviews will be written in.
     """
     reviews = product.get_reviews()
-    ensure_dir(directory)
+    ensure_dir(top_dir)
     for r in reviews:
-        with open('{0}/{1}'.format(directory, r.id), 'w') as r_f:
-            header = str(r)
-            r_f.write('{0}\n\n{1}'.format(header, r.text))
+        r_dir = '{0}/{1}'.format(top_dir, r.id)
+        ensure_dir(r_dir)
+        with open('{0}/content'.format(r_dir), 'w') as f:
+            f.write(r.text)
+
+        with open('{0}/info'.format(r_dir), 'w') as f:
+            info1 = '{0.id}\n{0.title}\n{0.stars}\n'
+            info2 = '{0.helpfulness}\n{0.product_id}\n{0.date}\n'
+            info = (info1 + info2).format(r)
+            f.write(info)
 
 def ensure_dir(d):
     """Ensure that the directory exists.
