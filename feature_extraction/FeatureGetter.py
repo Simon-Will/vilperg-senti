49846524068759ad5bd7e46dc8815dc7b77b2478
caro-berg <caroberg95@gmx.de>
2015-12-16 21:02:05 +0100

    added word class to FeatureGetter.py

--- feature_extraction/FeatureGetter.py
+++ feature_extraction/FeatureGetter.py
@@ -3,6 +3,8 @@
 
 # Abstract classes:
 
+import re
+
 class Feature_getter:
     pass
 
@@ -34,15 +36,13 @@ class Token_number_getter(Plain_feature_getter):
 
     def get_value(tagged_text):
         """Count the tokens in review.
-
         Args:
             tagged_text (list): A list of lists. Each of the inner lists
                 consists of three elements: token, POS-tag and lemma.
-
         Returns:
             count (int): The number of tokens in the review.
         """
-        count = 1 + 2 + 4 + 7 # looooool
+        count = len(tagged_text)
         return count
 
 class Type_number_getter(Plain_feature_getter):
@@ -56,13 +56,62 @@ class Overall_sentiment_getter(SentiWS_based_feature_getter):
         self.feature_type = 'integer'
 
 class Adjective_sentiment_getter(SentiWS_based_feature_getter):
-    pass
+    #consider all kinds of ADJ POS-Tags in tagged_text
+    pos = re. compile(r'ADJ.+')
+    
+    def __init__(senti_dict):
+        self.senti_dict = senti_dict
+        self.featurename = 'adjective_sentiment'
+        self.feature_type = 'integer'
+        
+    def get_value(tagged_text):
+        #sum of all adjective sentiments
+        value = 0
+        for item in tagged_text:
+            if pos.match(item[1]):
+                if item[0] in self.senti_dict:
+                    value += self.senti_dict[item[0]]
+        return value
+                    
 
 class Verb_sentiment_getter(SentiWS_based_feature_getter):
-    pass
+    #consider all kinds of VERB POS-Tags in tagged_text
+    pos = re.compile(r'V.+')
+    
+    def __init__(senti_dict):
+        self.senti_dict = senti_dict
+        self.featurename = 'verb_sentiment'
+        self. feature_type = 'integer'
+        
+    def get_value(tagged_text):
+        #sum of all verb sentiments
+        value = 0
+        for item in tagged_text:
+            if pos.match(item[1]):
+                if item[0] in self.senti_dict:
+                    value += self.senti_dict[item[0]]
+        return value
+    
 
 class Noun_sentiment_getter(SentiWS_based_feature_getter):
-    pass
+    #consider all kinds of NOUN POS-Tags in tagged_text
+    pos = re.compile(r'NN')
+    
+    def __init_(senti_dict):
+        self.senti_dict = senti_dict
+        self.featurename = 'noun_sentiment'
+        self.feature_type = 'integer'
+        
+    def get_value(tagged_text):
+        #sum of all noun sentiments
+        value = 0
+        for item in tagged_text:
+            if pos.match(item[1]):
+                if item[0] in self.senti_dict:
+                    value += self.senti_dict[item[0]]
+        return value
+        
+        
 
 class Keyword_feature_getter(Multi_feature_getter):
     def __init__(keywords):
