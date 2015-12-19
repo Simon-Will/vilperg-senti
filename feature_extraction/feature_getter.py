28ce72a25ac371395b5ba2b0b23627c2eda68a90
Simon-Will <will@cl.uni-heidelberg.de>
2015-12-19 17:22:57 +0100

    Finish write_features.py

--- feature_extraction/feature_getter.py
+++ feature_extraction/feature_getter.py
@@ -9,16 +9,16 @@ class Feature_getter:
     pass
 
 class Single_feature_getter(Feature_getter):
-    def __init__():
+    def __init__(self):
         self.feature_name = ''
         self.feature_type = ''
 
-    def get_value(review):
+    def get_value(self, review):
         # "Abstract" method.
         pass
 
 class SentiWS_based_feature_getter(Single_feature_getter):
-    def __init__(senti_dict):
+    def __init__(self, senti_dict):
         self.senti_dict = senti_dict
 
 class Plain_feature_getter(Single_feature_getter):
@@ -26,7 +26,7 @@ class Plain_feature_getter(Single_feature_getter):
 
 class Multi_feature_getter(Feature_getter):
 
-    def get_feature_triples(review):
+    def get_feature_triples(self, review):
         # "Abstract" method.
         pass
 
@@ -59,69 +59,69 @@ class Overall_sentiment_getter(SentiWS_based_feature_getter):
         self.feature_name = 'overall_sentiment'
         self.feature_type = 'numeric'
         
-    def get_overall_sentiment(self, tagged_text):
+    def get_value(self, tagged_text):
         value = 0
         for item in tagged_text:
-            if item[0] in self.senit_dict:
-                value += self.senit_dict[item[0]]
+            if item[0] in self.senti_dict:
+                value += self.senti_dict[item[0]]
         return value
 
 class Adjective_sentiment_getter(SentiWS_based_feature_getter):
-    #consider all kinds of ADJ POS-Tags in tagged_text
-    pos = re. compile(r'ADJ.+')
     
     def __init__(self, senti_dict):
         self.senti_dict = senti_dict
         self.feature_name = 'adjective_sentiment'
         self.feature_type = 'numeric'
+        #consider all kinds of ADJ POS-Tags in tagged_text
+        self.pos = re. compile(r'ADJ.+')
         
     def get_value(self, tagged_text):
         #sum of all adjective sentiments
         value = 0
         for item in tagged_text:
-            if pos.match(item[1]):
+            if self.pos.match(item[1]):
                 if item[0] in self.senti_dict:
                     value += self.senti_dict[item[0]]
         return value
 
 class Verb_sentiment_getter(SentiWS_based_feature_getter):
-    #consider all kinds of VERB POS-Tags in tagged_text
-    pos = re.compile(r'V.+')
     
     def __init__(self, senti_dict):
         self.senti_dict = senti_dict
         self.feature_name = 'verb_sentiment'
-        self. feature_type = 'numeric'
+        self.feature_type = 'numeric'
+        #consider all kinds of VERB POS-Tags in tagged_text
+        self.pos = re.compile(r'V.+')
         
     def get_value(self, tagged_text):
         #sum of all verb sentiments
         value = 0
         for item in tagged_text:
-            if pos.match(item[1]):
+            if self.pos.match(item[1]):
                 if item[0] in self.senti_dict:
                     value += self.senti_dict[item[0]]
         return value
     
 class Noun_sentiment_getter(SentiWS_based_feature_getter):
-    #consider all kinds of NOUN POS-Tags in tagged_text
-    pos = re.compile(r'NN')
     
-    def __init_(self, senti_dict):
+    def __init__(self, senti_dict):
         self.senti_dict = senti_dict
         self.feature_name = 'noun_sentiment'
         self.feature_type = 'numeric'
+        #consider all kinds of NOUN POS-Tags in tagged_text
+        self.pos = re.compile(r'NN')
         
     def get_value(self, tagged_text):
         #sum of all noun sentiments
         value = 0
         for item in tagged_text:
-            if pos.match(item[1]):
+            if self.pos.match(item[1]):
                 if item[0] in self.senti_dict:
                     value += self.senti_dict[item[0]]
         return value
 
 class Keyword_feature_getter(Multi_feature_getter):
-    def __init__(keywords):
+    def __init__(self, keywords):
         self.keywords = []
 
     def get_keywords(self, keywords):
--- feature_extraction/write_features.py
+++ feature_extraction/write_features.py
@@ -33,7 +33,7 @@ def get_files(dir_name, f_name):
             for root, dirs, files in os.walk(dir_name)
             for name in files
             if name == f_name]
-    return descriptionFiles
+    return files
 
 def read_review(in_file):
     """Read a review from a file and return it as a list of lists.
@@ -56,20 +56,21 @@ def get_review_features(feature_getters, review):
 
     Args:
         feature_getters (iterable): Several Feature_getter objects.
-        review (list): Lines with token, POS-tag and lemma on each line.
+        review (str): Full path to a review file.
 
     Returns:
         review_features (list): Features of the review. The list contains
             triples comprising feature_name, feature_value and feature_type.
     """
     review_features = []
+    review_content = read_review(review)
     for getter in feature_getters:
-        if isinstance(Single_feature_getter):
+        if isinstance(getter, fg.Single_feature_getter):
             feature_name = getter.feature_name
-            feature_value = getter.get_value(review)
+            feature_value = getter.get_value(review_content)
             feature_type = getter.feature_type
             review_features.append((feature_name, feature_value, feature_type))
-        elif isinstance(getter, Keyword_feature_getter):
+        elif isinstance(getter, fg.Keyword_feature_getter):
             pass
         else:
             error_message = 'Feature_getter "{0}" of unknown type ({1}).'
@@ -94,26 +95,72 @@ def make_feature_getters(features, senti_dict, getter_dict=FEATURE_GETTER_DICT):
         getters.append(eval(getter_dict[f]))
     return getters
 
-def write_review_features(review_features, out_file):
+def write_review_features(review_features, out_file, if_exists='append'):
     """Write review_features in out_file.
 
     Args:
         review_features (list): Features of the review. The list contains
             triples comprising feature_name, feature_value and feature_type.
         out_file (str): Full path to the file the features should be written to.
+        if_exists (str): String describing what to do if the out_file already
+            exists. Must be one of 'append', 'overwrite' and 'update'.
     """
-    with open(out_file) as f:
-        for rf in review_features:
-            line = '{0[0]}\t{0[1]}\t{0[2]}\n'.format(rf)
-            f.write(line)
+    line_pat = '{0[0]}\t{0[1]}\t{0[2]}\n'
+    if os.path.isfile(out_file):
+        if if_exists == 'append':
+            with open(out_file, 'a') as f:
+                for rf in review_features:
+                    line = line_pat.format(rf)
+                    f.write(line)
+        elif if_exists == 'overwrite':
+            with open(out_file, 'w') as f:
+                for rf in review_features:
+                    line = line_pat.format(rf)
+                    f.write(line)
+        elif if_exists == 'update':
+            old_lines = open(out_file, 'r').readlines()
+            new_lines = [line_pat.format(rf) for rf in review_features]
+            all_lines = []
+            for ol in old_lines:
+                in_new_lines = False
+                old_pat = re.compile(r'^{}\t.*$'.format(ol.split('\t')[0]))
+                for nl in new_lines:
+                    if old_pat.match(nl):
+                        in_new_lines = True
+                if not in_new_lines:
+                    all_lines.append(ol)
+            all_lines.extend(new_lines)
+            open(out_file, 'w').writelines(all_lines)
+        else:
+            error_pattern = '''Error: if_exists must be one of \'append\',
+                    \'overwrite\' and \'update\', but is {0}.'''
+            sys.stderr.write(error_pattern.format(if_exists))
+            sys.exit(3)
+    else:
+        with open(out_file, 'w') as f:
+            for rf in review_features:
+                line = line_pat.format(rf)
+                f.write(line)
 
-def write_all_review_features(feature_getters, reviews, out_base_name):
+def write_all_review_features(
+        feature_getters, reviews, out_base_name, if_exists='append'):
+    """Use feature_getters to get reviews and write them to files.
+
+    Args:
+        feature_getters (list): Feature_getter objects used to get features.
+        reviews (list): List of strings containing the full path to text files
+            containing the reviews.
+        out_base_name (str): Base name of the file the features should be
+            written to.
+        if_exists (str): String describing what to do if the out_file already
+            exists. Must be one of 'append', 'overwrite' and 'update'.
+    """
     out_file_pattern = '{0}/{1}'
     for r in reviews:
         # This will be a list of triples.
-        review_features = get_review_features(r)
+        review_features = get_review_features(feature_getters, r)
         out_file = out_file_pattern.format(os.path.dirname(r), out_base_name)
-        write_review_features(review_features, out_file)
+        write_review_features(review_features, out_file, if_exists)
 
 def main():
     d = 'Compute review features and write them to files.'
@@ -122,7 +169,7 @@ def main():
     parser.add_argument('--feature', '-f', action='append',
             required=True, help='A feature that is to be extracted.')
 
-    parser.add_argument('--in_file_name', '-i', default='content',
+    parser.add_argument('--in_file_name', '-i', default='content_tagged',
             help='''The name of the file the review text is stored in in each
             directory''')
 
@@ -130,35 +177,42 @@ def main():
             help='''The output file the features are written to in each
             directory.''')
 
-    parser.add_argument('--overwrite', action='store_true', default=False,
-            help='''If features were already computed for a review, forcibly
-            compute them again and replace the old value.''')
+    parser.add_argument('--if_exists',
+            choices=['overwrite', 'update', 'append'], default='append',
+            help='Decide what to do if the file out_file_name already exists.')
+
+    parser.add_argument('--sentiws_file', '-s', help='''The SentiWS file that
+            is used for the sentiment features.''')
 
     parser.add_argument('top_dir',
             help='''The directory that is searched recursively for files called
             in_file_name''')
 
     args = parser.parse_args()
-    print(args)
+    #print(args)
 
     top_dir = args.top_dir
     out_file = args.out_file_name
     in_file = args.in_file_name
     features = args.feature
-    overwrite = args.overwrite
+    if_exists = args.if_exists
+    sentiws_file = args.sentiws_file
 
     if in_file == out_file:
-        sys.stderr.write('Error: in_file_name cannot be the same as out_file_name.')
+        sys.stderr.write(
+                'Error: in_file_name cannot be the same as out_file_name.\n')
         sys.exit(1)
 
     for f in features:
         if f not in FEATURE_GETTER_DICT:
-            sys.stderr.write('Error: feature {f} is not a valid feature.'.format(f))
-            sys.exit(1)
+            error_pattern = 'Error: feature {0} is not a valid feature.\n'
+            sys.stderr.write(error_pattern.format(f))
+            sys.exit(2)
 
-    #reviews = get_files(top_dir, in_file)
-    #feature_getters = make_feature_getters(features)
-    #write_all_review_features(feature_getters, reviews, out_file)
+    senti_dict = SentiWS_handler.get_senti_dict(sentiws_file)
+    reviews = get_files(top_dir, in_file)
+    feature_getters = make_feature_getters(features, senti_dict)
+    write_all_review_features(feature_getters, reviews, out_file, if_exists)
 
 if __name__ == '__main__':
     main()
