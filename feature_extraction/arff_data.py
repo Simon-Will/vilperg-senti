4af4d3084b380018c58430ec7ff2ba8df3c513b1
caro-berg <caroberg95@gmx.de>
2015-12-22 13:19:33 +0100

    add module to create arff file

--- feature_extraction/arff_data.py
+++ feature_extraction/arff_data.py
@@ -1,36 +1,50 @@
-import arff
 
-#list of attributes
-arff_writer = arff.Writer(feature_example, relation = 'Review_sentiment',
-                            header_names = ['token_number',
-                                            'type_number',
-                                            'overall_sentiment',
-                                            'adjective_sentiment',
-                                            'verb_sentiment',
-                                            'noun_sentiment',
-                                            'keyword_sentiment',
-                                            'stars'])
-                                            
-arff_writer.pytypes[arff.numeric] = '{1,2,3,4,5}'
-arff_writer.write([arff.nominal('stars')])
+      
+import sys
+import os
+
+def get_files(dir_name, f_name):
+    """Recursively get regular files called f_name in the directory dir_name.
+    Args:
+        dir_name (str): The name of a directory.
+        f_name (str): The name of the files to be found.
+    Returns:
+        files (list): A list of strings containing the full paths to the files.
+    """
+    files = [os.path.join(root, name)
+            for root, dirs, files in os.walk(dir_name)
+            for name in files
+            if name == f_name]
+    return files 
+  
+dir_name = sys.argv[1]
+output_file = sys.argv[2]
+header_names = sys.argv[3:]
+
+   
+data = get_files(dir_name, 'features')
+arff_data = []
+
+for feature_file in data:
+  for line in open(feature_file):
+    line = line.strip()
+    list_line = line.split('\t')
+    if list_line[0] in header_names:
+      arff_data.append(list_line)
 
 
-#formate data from feature_example
-def write_to_list(feature_example):
-    data_input = []
-    arff_data = []
-    with open('feature_example') as feature_example:
-        for line in feature_example:
-            data_input.append(line.strip().split(','))
-    for lists in data_input:
-        for attribute_triple in lists:
-            triple_list = attribute_triple.split()
-        arff_data.append(triple_list)
-    return arff_data
+with open(output_file, 'w') as f:
+  #write relation to arff file
+  f.write("@relation {0}\n".format('Review_sentiment'))
+  #write attributes to arff file
+  for a in arff_data:
+    f.write("@attribute {0} {1}\n".format(a[0],a[2]))
+  #write data to arff file
+  for d in arff_data:
+    f.write("@data {0}\n".format(d[1]))
+  
+
+  
+  
     
-arff.dump(open(feature_example, 'w'), arff_data,
-                                    relation = 'Review_sentiment',
-                                    header_names)
-        
-
     
