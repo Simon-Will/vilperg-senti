7930242b587d79698d1b920dac0ccb6b4812d74d
Simon-Will <will@cl.uni-heidelberg.de>
2016-01-08 22:48:52 +0100

    Add utility script for parallel chunking

---
+++ review_chunking/make_chunks.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+CATEGORIES=( smartphones armbanduhren küche_haushalt baumaschinen )
+
+MLT="/home/students/will/semipublic/mlt/"
+MAKE_CHUNKS="$MLT/vilperg-senti/review_chunking/make_chunks.pl"
+
+for c in ${CATEGORIES[@]}
+do
+	CMD="$MAKE_CHUNKS\
+		--housing-dir reviews\
+		--without $MLT/peculiar_files/peculiar_${c}_3\
+		--chunk-size 50\
+		--balance\
+		$MLT/all_${c}_12_11_2015\
+		$MLT/chunks/cleansed_balanced_chunks_${c}"
+	
+	$CMD &
+done
+wait
