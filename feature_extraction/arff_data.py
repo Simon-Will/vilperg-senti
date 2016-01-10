5659b04e3213e47f20af3251f7111f802240ac3e
caro-berg <caroberg95@gmx.de>
2016-01-10 14:29:42 +0100

    fixed errors in arff_data.py

--- feature_extraction/arff_data.py
+++ feature_extraction/arff_data.py
@@ -19,30 +19,48 @@ def get_files(dir_name, f_name, symlinks=False):
 dir_name = sys.argv[1]
 output_file = sys.argv[2]
 header_names = sys.argv[3:]
-
-   
 data = get_files(dir_name, 'features', symlinks=True)
 arff_data = []
 
 for feature_file in data:
+  attribute_dict = {}
   for line in open(feature_file):
     line = line.strip()
     list_line = line.split('\t')
-    if list_line[0] in header_names:
-      arff_data.append(list_line)
+    for attribute in header_names:
+      if attribute == list_line[0]:
+        attribute_dict[attribute] = list_line[1]
+  if len(attribute_dict) == len(header_names):    
+    arff_data.append(attribute_dict)
 
 
+
+
+#attribute dictionary
+d = {'token_number':'numeric', 'type_number':'numeric', 'overall_sentiment':'numeric',
+        'adjective_sentiment':'numeric', 'verb_sentiment':'numeric',
+        'noun_sentiment':'numeric','keyword_sentiment':'numeric','stars':'{1,2,3,4,5}'}
+        
 with open(output_file, 'w') as f:
   #write relation to arff file
-  f.write("@relation {0}\n".format('Review_sentiment'))
+  f.write("@relation {0}\n\n\n".format('Review_sentiment'))
+  
   #write attributes to arff file
-  for a in arff_data:
-    f.write("@attribute {0} {1}\n".format(a[0],a[2]))
+  for attribute in header_names:
+    f.write ("@attribute {0} {1}\n".format(attribute,d[attribute]))         
+
   #write data to arff file
-  data_list = []
   f.write("@data\n")
+  data = []
   for d in arff_data:
-    data_list.append(d[1])
-  data_line = ','.join(data_list)
-  f.write(data_line)  
+    for attribute in header_names:
+      data.append(d[attribute])
+    
+    final_data = ','.join(data)
+    f.write(final_data)
+    f.write('\n')
+    data = []
+  
+  
+
 
