e193211da9ccf31f36b10d8617a1c186fcd5c0cf
Simon-Will <will@cl.uni-heidelberg.de>
2016-02-29 00:54:23 +0100

    Remove more old stuff

--- preprocessing/preprocess.sh
+++ preprocessing/preprocess.sh
@@ -13,17 +13,4 @@ FILENAME=$2
 
 MY_TREE_TAGGER="$HOME/semipublic/mlt/vilperg-senti/preprocessing/my_tree_tagger.sh"
 
-function tokenize {
-	FILE=$1
-	echo "here"
-	echo "$FILE"
-	$TOKENIZER -a $ABBR_LIST $FILE |
-	# external lexicon lookup
-	perl $CMD/lookup.perl $LEXFILE |
-	# tagging
-	$TAGGER $OPTIONS $PARFILE  | 
-	# error correction
-	$FILTER > "${1}_tagged"
-}
-
 find "$DIR" -name "$FILENAME" -exec $MY_TREE_TAGGER {} {}_tagged \;
